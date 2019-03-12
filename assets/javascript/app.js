383bb8d56f8f625bf1c97543c62274ffd774867d
KelseyGibbs <gibbs.kelseylee@gmail.com>
2019-03-12 01:51:35 -0500

    The minutes away function still needs to be written, as well as the read me

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-      // Initialize Firebase
+$(document).ready(function () {
+  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyD8vW1kYuGekSYhETM8MIlox6IHBRmrFug",
     authDomain: "project1-47baf.firebaseapp.com",
@@ -12,47 +12,76 @@ $(document).ready(function() {
 
   var database = firebase.database();
 
-$(".btn").on("click", function(event) {
+  $(".btn").on("click", function (event) {
     event.preventDefault();
-    
+
     var trainName = $("#nameInput").val().trim();
     var destination = $("#destinationInput").val().trim();
     var firstTrain = moment($("#timeInput").val().trim(), "HH:mm").format("HH:mm");
     var frequency = $("#minutesInput").val().trim();
     console.log(firstTrain);
 
-  // Creates local "temporary" object for holding train information
-  var newTrain = {
-    trainNameData: trainName,
-    destinationData: destination,
-    firstTrainData: firstTrain,
-    frequencyData: frequency
-  };
+    // Creates local "temporary" object for holding train information
+    var newTrain = {
+      trainNameData: trainName,
+      destinationData: destination,
+      firstTrainData: firstTrain,
+      frequencyData: frequency,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+    };
 
-  // Uploads employee data to the database
-  database.ref().push(newTrain);
+    // Uploads employee data to the database
+    database.ref().push(newTrain);
 
-  $("#nameInput").val("");
-  $("#destinationInput").val("");
-  $("#timeInput").val("");
-  $("#minutesInput").val("");
+    $("#nameInput").val("");
+    $("#destinationInput").val("");
+    $("#timeInput").val("");
+    $("#minutesInput").val("");
 
-});
+  });
 
-// Create Firebase event for adding train data to the database and a row in the html when a user adds an entry
+  // Create Firebase event for adding train data to the database and a row in the html when a user adds an entry
 
-database.ref().on("child_added", function(childSnapshot) {
+  database.ref().on("child_added", function (childSnapshot) {
     console.log(childSnapshot.val());
-  
+
     // Store everything into a variable.
     var trainNameData = childSnapshot.val().trainNameData;
+    var destinationData = childSnapshot.val().destinationData;
+    var firstTrainData = childSnapshot.val().firstTrainData;
+    var frequencyData = childSnapshot.val().frequencyData;
+    var thisDate = childSnapshot.val().dateAdded;
 
-  
     // Employee Info
     console.log(trainNameData);
+    console.log(destinationData);
+    console.log(firstTrainData);
+    console.log(frequencyData);
+    console.log(thisDate);
 
+    // Next Arrival
+    var nextArrival = moment().add(frequencyData, 'minutes').format("hh:mm");
+    
+    var minsAway = 9;
 
+    function howFar(){
 
-});
+    minsAway = 8;  
+      // Minutes Away
+    };
+
+    howFar();
+
+      var newRow = $("<tr>").append(
+        $("<td>").text(trainNameData),
+        $("<td>").text(destinationData),
+        $("<td>").text(frequencyData),
+        $("<td>").text(nextArrival),
+        $("<td>").text(minsAway)
+      );
+
+    $("table").append(newRow);
+
+  });
 
 });
